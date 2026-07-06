7c786de05824b2d34e4f8a7d30074a8c584826f9
Muhammad Rifqi Fameizy <rifqifam@Muhammads-MacBook-Air.local>
2026-07-06 11:29:00 +0700

    week 1 done

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,48 @@ public class Main {
     // we unpack every one of those in later weeks. For today, all you
     // need to know: execution begins on the line below.
     public static void main(String[] args) {
+
+        // Day 1
+
         String name = "Dune";
         int pages = 82;
         double price = 100.00;
         boolean availibility = true;
 
-        System.out.println("Title: " + name);
-        System.out.println("pages: " + pages);
-        System.out.println("price: " + price);
-        System.out.println("availibility: " + availibility);
+        // System.out.println("Title: " + name);
+        // System.out.println("pages: " + pages);
+        // System.out.println("price: " + price);
+        // System.out.println("availibility: " + availibility);
 
+
+        // Day 2
+
+        // if(availibility){
+        //     System.out.println("You can borrow it!");
+        // } else{
+        //     System.out.println("Book is not available to borrow");
+        // }
+
+        // if(pages<100){
+        //     System.out.println("This will be a quick read");
+        // } else if (pages<400){
+        //     System.out.println("A medium length read");
+        // } else{
+        //     System.out.println("Definitely a long read");
+        // }
+
+
+        // Day 3
+
+        int copies = 5;
+        for (int i = 1; i<= copies; i++){
+            System.out.println("Copy #" + i +" is ready to lend");
+        }
+
+        while(copies > 0){
+            System.out.println( copies + " Copies remaining");
+            copies--;
+        }
 
     }
 }
